096070df1f19862ed4873f4b50ad21689ac0558a
Intrejan <1157982786@qq.com>
2024-03-30 20:33:40 +0800

    fix: add HashSolution

--- learning/src/main/java/leetCode/HashSolution.java
+++ learning/src/main/java/leetCode/HashSolution.java
@@ -3,8 +3,11 @@ package leetCode;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class HashSolution {
 
@@ -144,7 +147,7 @@ public class HashSolution {
 
     public static List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        for(String s: strs) {
+        for (String s : strs) {
             String key = buildKey(s);
             if (!map.containsKey(key)) {
                 List<String> list = new ArrayList<>();
@@ -167,9 +170,74 @@ public class HashSolution {
         return Arrays.toString(count);
     }
 
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int complement = target - n;
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(n, i);
+        }
+        return new int[0];
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        int nextValue = n;
+        while (!set.contains(nextValue)) {
+            set.add(nextValue);
+            String str = String.valueOf(nextValue);
+            nextValue = 0;
+            for (char c : str.toCharArray()) {
+                nextValue += (int) Math.pow(Integer.parseInt(String.valueOf(c)), 2);
+            }
+            if (nextValue == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (map.containsKey(num)) {
+                if (Math.abs(map.get(num) - i) <= k) {
+                    return true;
+                }
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        int result = 0;
+        for (int num : nums) {
+            Set<Integer> left = findValue(set, num, -1);
+            Set<Integer> right = findValue(set, num, 1);
+            result = Math.max(left.size() + right.size() + 1, result);
+            set.remove(num);
+            set.removeAll(left);
+            set.removeAll(right);
+        }
+        return result;
+    }
+
+    public static Set<Integer> findValue(Set<Integer> set, int num, int addition) {
+        Set<Integer> subSet = new HashSet<>();
+        int nextValue = num + addition;
+        while (set.contains(nextValue)) {
+            subSet.add(nextValue);
+            nextValue += addition;
+        }
+        return subSet;
+    }
     public static void main(String[] args) {
-        HashSolution hashSolution = new HashSolution();
-        int[] A = {4, 5, 0, -2, -3, 1};
-        System.out.println(hashSolution.subarraysDivByK(A, 5));
+        System.out.println(longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
     }
 }
