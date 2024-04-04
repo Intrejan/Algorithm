3137bb27894cb965efe62f623b06ec8c12eb19ca
Intrejan <1157982786@qq.com>
2024-04-04 11:50:52 +0800

    fix: add 链表

--- learning/src/main/java/leetCode/LinkedSolution.java
+++ learning/src/main/java/leetCode/LinkedSolution.java
@@ -217,12 +217,45 @@ public class LinkedSolution {
         return pre.next;
     }
 
-//    public static ListNode reverseBetween(ListNode head, int left, int right) {
-//
-//    }
+    public static ListNode reverseKGroup2(ListNode head, int k) {
+        ListNode start = new ListNode();
+        start.next = head;
+        ListNode pre = start;
+        ListNode curr = head;
+        while (curr != null) {
+            for (int i = 0; i < k; i++) {
+                if (curr == null) {
+                    return start.next;
+                }
+                curr = curr.next;
+            }
+            pre = reverseK(pre, pre.next, k - 1);
+        }
+        return start.next;
+    }
+
+    public static ListNode reverseK(ListNode pre, ListNode curr, int count) {
+        for (int i = 0; i < count; i++) {
+            ListNode next = curr.next;
+            curr.next = next.next;
+            next.next = pre.next;
+            pre.next = next;
+        }
+        return curr;
+    }
+
     public static void main(String[] args) {
-        LinkedSolution linkedSolution = new LinkedSolution();
-        //System.out.println(linkedSolution.generateParenthesis(2));
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = null;
+        reverseKGroup2(n1, 2);
     }
 }
 
